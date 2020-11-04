db55c40288729336d5972b6ba6241a574e1f1783
Nicole Beatriz <nick_beatriz_@hotmail.com>
2020-11-03 23:12:56 -0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV == 'production') {
-    module.exports = { mongoURI: 'mongodb+srv://nih:admin@cluster0.mongodb.net/test?retryWrites=true&w=majority' }
+    module.exports = { mongoURI: 'mongodb+srv://weather-app:eMCztiZ5YSxSLaEW@cluster0.qmxnh.mongodb.net/weatherapp?retryWrites=true&w=majority' }
 } else {
-    module.exports = { mongoURI: 'mongodb://localhost/blogapp' }
+    module.exports = { mongoURI: 'mongodb://localhost/weatherapp' }
 }
